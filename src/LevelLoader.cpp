5cfb55d44c3b1f08615eb71750876ef969c203e6
Evyn <evyn.brouwer@ontariotechu.net>
2019-11-05 16:42:58 -0500

    [Edit] More Level Loading

--- src/LevelLoader.cpp
+++ src/LevelLoader.cpp
@@ -13,7 +13,17 @@ LevelLoader::LevelLoader(const char* filename)
 	{
 		char line[1024];
 		int lineNumber = fscanf(file, "%s", line);
-		if (lineNumber == EOF)
+		if (lineNumber == EOF){//if end of file
+			moreFile = false;
+		}
+		else if (strcmp(line, "o") == 0){//if new object
+			fscanf(file, "%s\n", &tempName);//get name of object
+		}
+		else if (strcmp(line, "v") == 0){//if vertex
+			glm::vec3 vertex;
+			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+		}
+		else if (strcmp(line, "s") == 0)//end of new object
 		{
 
 		}
