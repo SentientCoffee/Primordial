d9e3391c2348d565c690eb59d14754dcaf5b8b80
Evyn <evyn.brouwer@ontariotechu.net>
2019-11-06 19:02:59 -0500

    [Edit] Level Loading info

--- src/LevelLoader.cpp
+++ src/LevelLoader.cpp
@@ -22,10 +22,41 @@ LevelLoader::LevelLoader(const char* filename)
 		else if (strcmp(line, "v") == 0){//if vertex
 			glm::vec3 vertex;
 			fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+			_tempVerts.push_back(vertex);
 		}
 		else if (strcmp(line, "s") == 0)//end of new object
 		{
-
+			if (tempName[0] == 'E')
+				exits.push_back(findCenter());
+			else if (tempName[0] == 'D')
+				entrance = findCenter();
+			_tempVerts.clear();
 		}
 	}
+}
+
+glm::vec3 LevelLoader::findCenter()
+{
+	glm::vec3 tempHigh = _tempVerts[0];
+	glm::vec3 tempLow = _tempVerts[0];
+	for (unsigned int i = 0; i < _tempVerts.size(); i++)
+	{
+		if (_tempVerts[i].x > tempHigh.x)
+			tempHigh.x = _tempVerts[i].x;
+		if (_tempVerts[i].y > tempHigh.y)
+			tempHigh.y = _tempVerts[i].y;
+		if (_tempVerts[i].z > tempHigh.z)
+			tempHigh.z = _tempVerts[i].z;
+
+		if (_tempVerts[i].x < tempLow.x)
+			tempLow.x = _tempVerts[i].x;
+		if (_tempVerts[i].y < tempLow.y)
+			tempLow.y = _tempVerts[i].y;
+		if (_tempVerts[i].z < tempLow.z)
+			tempLow.z = _tempVerts[i].z;
+	}
+
+	return glm::vec3(tempHigh.x / 2 + tempLow.x / 2,
+		tempHigh.y / 2 + tempLow.y / 2,
+		tempHigh.z / 2 + tempLow.z / 2);
 }
