97d02fe4c6a2a7fee0d7837846ee95f4dde99773
Anthony <anth19800@hotmail.com>
2019-11-08 22:42:42 -0500

    [Add] Review Commit

--- src/GameplayScene711.cpp
+++ src/GameplayScene711.cpp
@@ -13,7 +13,7 @@ GameplayScene::GameplayScene(bool isActive)
 	_testEnemy->_transform.scale(glm::vec3(1.0f, 1.0f, 1.0f), 0.5f);
 
 
-	_floorObject = new Building("./Assets/LevelData/Level1Data.obj","./Assets/Meshes/Hitboxes/floorHitBox.obj",&_pLight._pointLightShader, std::vector<Cappuccino::Texture*>{ diffuse,spec }, std::vector<Cappuccino::Mesh*>{ new Cappuccino::Mesh("room1.obj") });
+	_floorObject = new Building("./Assets/LevelData/Level1Data.obj","./Assets/Meshes/Hitboxes/floorHitBox.obj",&_pLight._pointLightShader, std::vector<Cappuccino::Texture*>{ diffuse,spec }, std::vector<Cappuccino::Mesh*>{ new Cappuccino::Mesh("room2.obj") });
 
 	//init members here
 	auto mesh = new Cappuccino::Mesh("Bullet.obj");
