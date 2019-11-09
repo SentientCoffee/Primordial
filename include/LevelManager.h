b332dfac4fc46ca58fd06f3b9de4ca21527ebe8a
Evyn <evyn.brouwer@ontariotechu.net>
2019-11-09 16:29:58 -0500

    [Edit] Level Data Loader

--- include/LevelManager.h
+++ include/LevelManager.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "Building.h"
+#include "Cappuccino/RigidBody.h"
 class LevelManager
 {
 public:
+	LevelManager();
+	void update(float dt,Cappuccino::RigidBody player);
 
 
 private:
 
 
 	std::vector <Building> rooms;
+	std::vector <Building> airlocks;
 };
