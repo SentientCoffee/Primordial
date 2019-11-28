a424c53a5e6627db76854c54718e26b7e587e57b
Synral <daniel.hong1@ontariotechu.net>
2019-11-27 19:29:51 -0500

    [WIP] CmR Sentry
    - Interesting movement
    - Technically has a catmull rom spline movement, but not the intended way
    - Look at test1, test2, and test3 for further detail inside the CatmullRom function in Enemy.cpp

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -210,27 +210,50 @@ void Sentry::attack(Class* other, float dt)
 	float dist = glm::length(newPos);
 
 	auto normOther = glm::normalize(newPos);
-	auto perp = glm::normalize(glm::cross(newPos / 2.0f, normOther));
+	auto perp = glm::normalize(glm::cross(other->_rigidBody._position, normOther));
+	//auto dottest = glm::dot(normOther, perp); //resulted in 0 so it is perpendicular
 
 	//Uniform Catmull Rom Spline (Closed Loop)
-	glm::vec3 dir = glm::normalize(CatmullRom(0.2f, 
-		other->_rigidBody._position - 5.0f * normOther, 
-		other->_rigidBody._position - 5.0f * perp, 
-		other->_rigidBody._position + 5.0f * normOther, 
-		other->_rigidBody._position + 5.0f * perp) - _rigidBody._position);
-	
+	// Logically, I get the catmull rom position 0.2 along the curve, subtract position of sentry to get vector, 
+	//normalize to get direction, then apply dir to sentry's velocity
+	glm::vec3 crmPos = CatmullRom(0.2f,
+		other->_rigidBody._position - (20.0f * normOther),
+		other->_rigidBody._position - (20.0f * perp),
+		other->_rigidBody._position + (20.0f * normOther),
+		other->_rigidBody._position + (20.0f * perp));
+
+	glm::vec3 dir = glm::normalize(crmPos - _rigidBody._position);
+
 	_rigidBody.setVelocity(dir * 5.0f);
-	
-	_enemyGun->shoot(glm::vec3(normOther), _rigidBody._position);	
+
+	_enemyGun->shoot(glm::vec3(normOther), _rigidBody._position);
 
 }
 
 glm::vec3 Enemy::CatmullRom(float t, glm::vec3 p0, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3)
 {
-	return (0.5f * ((2.0f * p1) +
+	glm::mat4x4 catmull = { {-1.0f, 3.0f, -3.0f, 1.0f},
+							{2.0f, -5.0f, 4.0f, -1.0f},
+							{-1.0f, 0.0f, 1.0f, 0.0f},
+							{0.0f, 2.0f, 0.0f, 0.0f} };
+
+	glm::vec4 curve = { 1.0f, t, t * t, t * t * t };
+
+	glm::mat4x3 waypoints = { p0, p1, p2, p3 };
+
+	auto test1 = (0.5f * ((2.0f * p1) +
 		((-p0 + p2) * t) +
 		((2.0f * p0 - 5.0f * p1 + 4.0f * p2 - p3) * (t * t)) +
-		((-p0 + 3.0f * p1 - 3.0f * p2 + p3) * (t * t * t))));
+		((-p0 + 3.0f * p1 - 3.0f * p2 + p3) * (t * t * t)))); //reverse order of the catmull matrix but seems to circle the player extremely closely regardless of p0->p3 position
+	
+
+	auto test2 = 0.5f * (curve * catmull * waypoints); //gets stuck 
+
+
+	auto test3 = 0.5f * (waypoints * catmull * curve); //circles a spot near the light?
+
+
+	return test1;
 }
 
 void Sentry::wander(float dt)
