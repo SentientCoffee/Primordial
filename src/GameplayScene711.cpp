b7976a72a16c87eee746c0877e64962c58648f86
Anthony <anth19800@hotmail.com>
2020-02-11 01:13:35 -0500

    [Add] Room1 textures

--- src/GameplayScene711.cpp
+++ src/GameplayScene711.cpp
@@ -107,10 +107,13 @@ GameplayScene::GameplayScene(const bool isActive) :
 
 	//handle room data here
 
+	auto _levelDiffuse =		 LOAD_TEXTURE("room1 diffuse", "room/room1_low_DefaultMaterial_BaseColor.png",Cappuccino::TextureType::DiffuseMap);
+	auto _levelSpecular =		 LOAD_TEXTURE("room1 diffuse", "room/room1_low_DefaultMaterial_BaseColor.png",Cappuccino::TextureType::SpecularMap);
+	auto _levelNormal =			 LOAD_TEXTURE("room1 diffuse", "room/room1_low_DefaultMaterial_Normal.png",Cappuccino::TextureType::NormalMap);
 
-	_levelManager.rooms.push_back(new Building("./Assets/LevelData/Room2LevelData.obj", "./Assets/SpawnData/Room2SpawnData.obj", "./Assets/Meshes/Hitboxes/Room2HitboxData.obj", &_pLight._pointLightShader, { diffuse, spec }, { LOAD_MESH("Room 2", "Room2/Room2_Low.obj") }));
-	_levelManager.rooms.push_back(new Building("./Assets/LevelData/Room1LevelData.obj", "./Assets/SpawnData/Room1SpawnData.obj", "./Assets/Meshes/Hitboxes/Room1HitboxData.obj", &_pLight._pointLightShader, { diffuse, spec }, { LOAD_MESH("Room 1", "Room1/Room1_Low.obj") }));
-	_levelManager.rooms.push_back(new Building("./Assets/LevelData/Room3LevelData.obj", "./Assets/SpawnData/Room3SpawnData.obj", "./Assets/Meshes/Hitboxes/Room3HitboxData.obj", &_pLight._pointLightShader, { diffuse, spec }, { LOAD_MESH("Room 3", "Room3/Room3_low.obj") }));
+	_levelManager.rooms.push_back(new Building("./Assets/LevelData/Room1LevelData.obj", "./Assets/SpawnData/Room1SpawnData.obj", "./Assets/Meshes/Hitboxes/Room1HitboxData.obj", &_pLight._pointLightShader, { _levelDiffuse,_levelSpecular,_levelNormal }, { LOAD_MESH("Room 1", "Room1/Room1_Low.obj") }));
+	_levelManager.rooms.push_back(new Building("./Assets/LevelData/Room2LevelData.obj", "./Assets/SpawnData/Room2SpawnData.obj", "./Assets/Meshes/Hitboxes/Room2HitboxData.obj", &_pLight._pointLightShader, { diffuse,spec }, { LOAD_MESH("Room 2", "Room2/Room2_Low.obj") }));
+	_levelManager.rooms.push_back(new Building("./Assets/LevelData/Room3LevelData.obj", "./Assets/SpawnData/Room3SpawnData.obj", "./Assets/Meshes/Hitboxes/Room3HitboxData.obj", &_pLight._pointLightShader, { diffuse,spec }, { LOAD_MESH("Room 3", "Room3/Room3_low.obj") }));
 
 	for (unsigned i = 0; i < 5; i++)
 		_levelManager.airlocks.push_back(new Building("./Assets/LevelData/AirLockLevelData.obj", "./Assets/SpawnData/AirLockSpawnData.obj", "./Assets/Meshes/Hitboxes/AirlockHitboxData.obj", &_pLight._pointLightShader, { matte, spec }, { LOAD_MESH("Airlock", "Airlock.obj") }));
